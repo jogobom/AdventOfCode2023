15d1bd4dc5026d521c51c549392c7fe826ee11c5
Chris Preston <Chris_Preston@waters.com>
2023-12-14 16:34:11 +0000

    FINALLY FINISHED DAY 4!!!

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,23 +1,44 @@
-mod scratchcard;
+use std::fs::File;
+use std::io::{BufReader, Lines};
 
-use crate::scratchcard::Scratchcard;
 use utils::file::read_lines;
 
+use crate::scratchcard::Scratchcard;
+
+mod scratchcard;
+
 fn main() {
-    let mut total = 0;
-    // let mut sum_of_powers = 0;
+    let input_lines = read_lines("input").expect("Could not read input file");
 
-    if let Ok(lines) = read_lines("input") {
-        for line_result in lines {
-            if let Ok(line) = line_result {
-                let scratchcard = Scratchcard::from_string(&line);
+    let scratchcards = build_scratchcards(input_lines);
+    let copies_to_make = calculate_wins(&scratchcards);
+    let counted_cards = scratchcards.iter().zip(copies_to_make.iter());
 
-                println!("Scratchcard {:?}", scratchcard);
+    println!(
+        "total number of scratchcards = {:?}",
+        counted_cards.fold(0, |acc, s| acc + s.1)
+    );
+}
 
-                total += scratchcard.points_value()
-            }
+fn build_scratchcards(input_lines: Lines<BufReader<File>>) -> Vec<Scratchcard> {
+    let mut scratchcards = Vec::new();
+    for line_result in input_lines {
+        if let Ok(line) = line_result {
+            scratchcards.push(Scratchcard::from_string(&line));
         }
     }
-    println!("total points = {total}");
-    // println!("sum of powers = {sum_of_powers}")
+    scratchcards
+}
+
+fn calculate_wins(scratchcards: &Vec<Scratchcard>) -> Vec<usize> {
+    let mut future_card_copies = vec![1; scratchcards.len()];
+    for (i, scratchcard) in scratchcards.iter().enumerate() {
+        let num_wins = scratchcard.number_of_wins();
+        let begin = i + 1;
+        let end = i + 1 + num_wins;
+        for n in begin..end {
+            future_card_copies[n] += future_card_copies[i]
+        }
+    }
+    future_card_copies
 }
--- day4/src/scratchcard.rs
+++ day4/src/scratchcard.rs
@@ -1,23 +1,21 @@
 use regex::{Match, Regex};
 
-#[derive(Debug, PartialEq, Default)]
+#[derive(Debug, PartialEq, Default, Clone)]
 pub struct Scratchcard {
-    pub number: u32,
+    pub number: usize,
+    pub copies: usize,
     pub winners: Vec<u32>,
     pub have: Vec<u32>,
 }
 
 impl Scratchcard {
-    pub(crate) fn points_value(&self) -> u64 {
+    pub(crate) fn number_of_wins(&self) -> usize {
         let matched_numbers = self
             .have
             .iter()
             .filter(|h| self.winners.contains(&h))
-            .count() as u32;
-        match matched_numbers {
-            0 => 0,
-            _ => 2u64.pow(matched_numbers - 1),
-        }
+            .count();
+        matched_numbers
     }
 
     fn collect_captures(captures: Option<Match>) -> Vec<&str> {
@@ -37,12 +35,13 @@ impl Scratchcard {
 
         let number = caps
             .name("number")
-            .map_or(0u32, |m| m.as_str().parse::<u32>().unwrap());
+            .map_or(0, |m| m.as_str().parse::<usize>().unwrap());
         let winners: Vec<&str> = Self::collect_captures(caps.name("winners"));
         let have: Vec<&str> = Self::collect_captures(caps.name("have"));
 
         Scratchcard {
             number,
+            copies: 1,
             winners: winners.iter().map(|w| w.parse::<u32>().unwrap()).collect(),
             have: have.iter().map(|h| h.parse::<u32>().unwrap()).collect(),
         }
